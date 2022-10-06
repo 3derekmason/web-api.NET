7801790c63e0a3c3d5b81cfda97f4f9ec284571a
Derek Mason <3derekmason@gmail.com>
2022-10-06 09:05:04 -0600

    build: post, put, delete

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -12,13 +12,13 @@ public class PizzaController : ControllerBase
     {
     }
 
-    // GET all action
-    [HttpGet]
+// GET all action
+[HttpGet]
 public ActionResult<List<Pizza>> GetAll() =>
     PizzaService.GetAll();
 
-    // GET by Id action
-    [HttpGet("{id}")]
+// GET by Id action
+[HttpGet("{id}")]
 public ActionResult<Pizza> Get(int id)
 {
     var pizza = PizzaService.Get(id);
@@ -29,9 +29,41 @@ public ActionResult<Pizza> Get(int id)
     return pizza;
 }
 
-    // POST action
+// POST action
+[HttpPost]
+public IActionResult Create(Pizza pizza)
+{
+    PizzaService.Add(pizza);
+    return CreatedAtAction(nameof(Create), new { id = pizza.Id }, pizza);
+}
 
-    // PUT action
+// PUT action
+[HttpPut("{id}")]
+public IActionResult Update(int id, Pizza pizza)
+{
+    if (id != pizza.Id)
+    return BadRequest();
 
-    // DELETE action
+    var existingPizza = PizzaService.Get(id);
+    if(existingPizza is null)
+        return NotFound();
+
+    PizzaService.Update(pizza);
+
+    return NoContent();
+}
+
+// DELETE action
+[HttpDelete("{id}")]
+public IActionResult Delete(int id)
+{
+    var pizza = PizzaService.Get(id);
+
+    if (pizza is null)
+        return NotFound();
+
+    PizzaService.Delete(id);
+
+    return NoContent();
+}
 }
